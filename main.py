0f6f0496fb8ab12627b60092235449da30a643e8
Andrey Yudin <yudin_ag@ursip.ru>
2023-10-08 18:06:47 +0300

    2,3,4

--- main.py
+++ main.py
@@ -7,6 +7,9 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi1, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi3, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi4, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
